c81cde4451ca304ff9fb6dde51b89f7a9e48f60b
Lautaro <fayanaslautaroagustin@gmail.com>
2022-12-05 22:31:47 -0300

    primerCommit

--- src/components/AppClimax.jsx
+++ src/components/AppClimax.jsx
@@ -1,10 +1,18 @@
+import useClima from '../hooks/useClima'
 import { Formulario } from './Formulario'
+import { Resultado } from './Resultado'
 
 export const AppClimax = () => {
+
+  const { resultado } = useClima()
+
   return (
     <>
         <main className='dos-columnas'>
                 <Formulario />
+
+                {resultado?.name && <Resultado /> }
+                
         </main>
     </>  
     )
--- src/context/ClimaProvider.jsx
+++ src/context/ClimaProvider.jsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { createContext, useState } from "react";
 
 const ClimaContext = createContext();
@@ -8,6 +9,8 @@ export const ClimaProvider = ({children}) => {
         ciudad: '' ,
         pais: ''
     })
+
+    const [ resultado , setResultado ] = useState({})
     
     const datosBusqueda = e => {
         setBusqueda({
@@ -16,14 +19,28 @@ export const ClimaProvider = ({children}) => {
         })
     }
 
-    const consultarClima = datos => {
-        console.log(datos);
+    const consultarClima = async datos => {
+        try {
+            
+         const { ciudad , pais } = datos
+         const appId = import.meta.env.VITE_API_KEY
+         const url = `http://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=1&appid=${appId}`
+         const { data } = await axios(url)
+         console.log(data[0]);
+         const { lat , lon } = data[0]
+         const urlClima = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`
+         const { data: clima } = await axios(urlClima)
+         setResultado(clima);
+        } catch (error) {
+            console.log(error);
+        }
     }
 
   return (
         <ClimaContext.Provider
             value={{
                 busqueda,
+                resultado,
                 datosBusqueda,
                 consultarClima
             }}
